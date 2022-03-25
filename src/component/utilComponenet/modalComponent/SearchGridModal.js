abca3bc539618370a06d3372dcd3463efd0eadb6
wjsalsgus1 <97424180+wjsalsgus1@users.noreply.github.com>
2022-03-25 11:00:35 +0900

    update

--- src/component/utilComponenet/modalComponent/SearchGridModal.js
+++ src/component/utilComponenet/modalComponent/SearchGridModal.js
@@ -2,20 +2,23 @@ import React from "react";
 import ModalComponent from "./ModalComponent";
 import Form from "../formComponent/FormComponent";
 
-const Modal = ({ open, closeModal, changeModalForm, addModalColumn, data }) => {
+const Modal = ({ open, closeModal, changeModalForm, addModalColumn, updateModalColumn, updateModal, addModal, data }) => {
   return (
     <div>
       <ModalComponent
         open={open}
         close={closeModal}
         addModalColumn={addModalColumn}
+        updateModalColumn={updateModalColumn}
+        updateModal={updateModal}
+        addModal={addModal}
         header="first modal"
         main={
           <>
             <Form
               items={[
-                { value: { name: "gridname", text: "그리드명"}},
-                { value: { name: "dataApi", text: "대이터호출API"}}
+                { value: { name: "gridname", text: "그리드명", content: data.gridname}},
+                { value: { name: "dataApi", text: "대이터호출API", content: data.dataApi}}
               ]}
               changeForm={changeModalForm}
               readOnly={false}
